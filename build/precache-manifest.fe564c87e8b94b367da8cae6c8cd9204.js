e1363110624b0ec9143444e2b5d6ca08a1c865af
kasimiraula <kasimir.aula@helsinki.fi>
2020-04-30 18:24:13 +0300

    frontend added

---
+++ build/precache-manifest.fe564c87e8b94b367da8cae6c8cd9204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af92e6c9de81300f5054c3e463f067d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0da570c68781528ccc5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5e6fb33ea3d4ea39c312",
+    "url": "/static/js/2.d1e4bd8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d1e4bd8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0da570c68781528ccc5",
+    "url": "/static/js/main.1ad57c64.chunk.js"
+  },
+  {
+    "revision": "7ea0722f074be41be6c9",
+    "url": "/static/js/runtime-main.a8065958.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
