8847743113b54d770f18aa8e86bd653c199d3d47
kasimiraula <kasimir.aula@helsinki.fi>
2020-04-30 18:19:41 +0300

    fixing confs for heroku

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ let items = [
     response.json(item)
   })
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
