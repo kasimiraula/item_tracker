8a28ede4562ace20ca7f772e682b0371391efa15
kasimiraula <kasimir.aula@helsinki.fi>
2020-05-02 15:50:39 +0300

    missing files added to frontend

---
+++ build/precache-manifest.14dc54ff4b4c761a39ba615a95286e6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3a9818b730371fe0851ab2d560efe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16ae6f750d7c716357b5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5e6fb33ea3d4ea39c312",
+    "url": "/static/js/2.d1e4bd8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d1e4bd8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ae6f750d7c716357b5",
+    "url": "/static/js/main.efd76925.chunk.js"
+  },
+  {
+    "revision": "7ea0722f074be41be6c9",
+    "url": "/static/js/runtime-main.a8065958.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
